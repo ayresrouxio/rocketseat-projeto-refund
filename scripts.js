e08d9bb9ad8f7b10dc27c3a547fe73a968051871
Ayres Roux <ayresrouxio@gmail.com>
2024-12-17 20:56:41 -0300

    aula: formatando a moeda

--- scripts.js
+++ scripts.js
@@ -2,5 +2,15 @@ const amount = document.querySelector("#amount")
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D+/g, "")
-  amount.value = value
+
+  value = Number(value) / 100 // Valor em centavos
+
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  return value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
 }
