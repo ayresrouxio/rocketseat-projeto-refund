98ec2c017a8e84568fad43b0c5308d04b80c3f4f
Ayres Roux <ayresrouxio@gmail.com>
2024-12-18 19:43:53 -0300

    aula: calculando o valor total

--- scripts.js
+++ scripts.js
@@ -4,7 +4,8 @@ const expense = document.querySelector("#expense")
 const category = document.querySelector("#category")
 
 const expenseList = document.querySelector("ul")
-const expensesQuantity = document.querySelector("aside header p span")
+const expenseQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D+/g, "")
@@ -87,16 +88,31 @@ function updateTotals() {
   try {
     const items = expenseList.children
 
-    expensesQuantity.textContent = `${items.length} ${
+    expenseQuantity.textContent = `${items.length} ${
       items.length > 1 ? "despesas" : "despesa"
     }`
 
     let total = 0
+
     for (let item = 0; item < items.length; item++) {
       const itemAmount = items[item].querySelector(".expense-amount")
+
+      let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",", ".")
+
+      value = parseFloat(value)
+
+      if (isNaN(value)) {
+        return alert(
+          "Não foi possível calcular o total. O valor não parece ser um número."
+        )
+      }
+
+      total += Number(value)
     }
+
+    expensesTotal.textContent = total
   } catch (error) {
-    console.log(error)
     alert("Não foi possível atualizar os totais.")
+    console.log(error)
   }
 }
