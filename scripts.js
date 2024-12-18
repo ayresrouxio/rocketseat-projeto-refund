b4cac4a08a27b7368f5de807694edc1607604749
Ayres Roux <ayresrouxio@gmail.com>
2024-12-18 20:07:43 -0300

    aula: removendo um item da lista

--- scripts.js
+++ scripts.js
@@ -128,6 +128,9 @@ function updateTotals() {
 
 expenseList.addEventListener("click", function (event) {
   if (event.target.classList.contains("remove-icon")) {
-    console.log(event)
+    const item = event.target.closest(".expense")
+    item.remove()
   }
+
+  updateTotals()
 })
